b0e669151bdb56b119b17d5ee17946d7fe3c656c
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-05-30 13:19:37 +0530

    testimonials added

--- src/Views/Testimonials.js
+++ src/Views/Testimonials.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
 import Narrow from "../Components/Common/Narrow";
 import { Row, Col, Button } from "reactstrap";
-import img from "../Images/jeevshakti.png"; // Ensure this path is correct
 import Getintouch from "../Components/Getintouch";
 
 export default function Testimonials() {
   const [isVisible, setIsVisible] = useState(false);
+  const isLargeScreen = () => window.innerWidth > 1024;
+  const [isLarge, setIsLarge] = useState(isLargeScreen());
+  useEffect(() => {
+    // Function to handle resize event
+    const handleResize = () => {
+      setIsLarge(isLargeScreen());
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     setIsVisible(true);
@@ -37,51 +52,54 @@ export default function Testimonials() {
         </div>
       </Narrow>
 
-      <div className="card1 bg-[#F3F6FD] mb-32 py-10 ">
-        <Narrow>
-          <div className="jeevshaktibg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 max-xl:p-10 group rounded-xl">
-            <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
-            <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
-              <div className="h-full w-full">
-                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
-                  Jeevshakti
-                </h1>
-                <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
-                  Jeevshakti Foundation, a charitable trust established in 2017,
-                  is committed to the welfare of stray animals in Delhi. Through
-                  initiatives like feeding, medical care, and sterilizations,
-                  they aim to improve the lives of these animals. Their
-                  innovative solutions and community involvement reflect their
-                  dedication to alleviating animal suffering and promoting
-                  compassion.
-                </p>
-                <div className="text-center mt-3">
-                  <a
-                    href="http://jeevshaktifoundation.org/"
-                    target="_blank"
-                    className="bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-full"
-                  >
-                    Visit Website
-                  </a>
+      <div className="card1 px-48 max-xl:px-2 bg-[#F3F6FD] mb-32 py-10 ">
+
+          <div
+            className={` ${isLarge ? " stickycard1" : " "
+              } `}
+          >
+            <div className="jeevshaktibg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 max-xl:p-10 group rounded-xl">
+              <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
+              <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
+                <div className="h-full w-full">
+                  <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
+                    Jeevshakti
+                  </h1>
+                  <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
+                    Jeevshakti Foundation, a charitable trust established in 2017,
+                    is committed to the welfare of stray animals in Delhi. Through
+                    initiatives like feeding, medical care, and sterilizations,
+                    they aim to improve the lives of these animals. Their
+                    innovative solutions and community involvement reflect their
+                    dedication to alleviating animal suffering and promoting
+                    compassion.
+                  </p>
+                  <div className="text-center mt-3">
+                    <a
+                      href="http://jeevshaktifoundation.org/"
+                      target="_blank"
+                      className="bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-full"
+                    >
+                      Visit Website
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <div
+            className={` ${isLarge ? " stickycard1" : " "
+              } `}
+          >
           <div className="medecombg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 mt-10 max-xl:p-10 group rounded-xl">
             <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
             <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
               <div className="h-full w-full">
                 <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
-                  Jeevshakti
+                  Medecom
                 </h1>
                 <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
-                  Jeevshakti Foundation, a charitable trust established in 2017,
-                  is committed to the welfare of stray animals in Delhi. Through
-                  initiatives like feeding, medical care, and sterilizations,
-                  they aim to improve the lives of these animals. Their
-                  innovative solutions and community involvement reflect their
-                  dedication to alleviating animal suffering and promoting
-                  compassion.
+                Medecom is a renowned provider of medical imaging software, specializing in Radiology and Mammography since 2000. Their solutions seamlessly integrate into radiology centers, clinics, and hospitals, offering ergonomic interfaces, optimized workflows, and secure exchanges. Medecom's mission is to facilitate study acquisition, diagnosis, and communication within radiology departments, ensuring efficient and effective processes.
                 </p>
                 <div className="text-center mt-3">
                   <a
@@ -95,6 +113,11 @@ export default function Testimonials() {
               </div>
             </div>
           </div>
+          </div>
+          <div
+            className={` ${isLarge ? " stickycard1" : " "
+              } `}
+          >
           <div className="jeevshaktibg mt-10 relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 max-xl:p-10 group rounded-xl">
             <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
             <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
@@ -123,21 +146,20 @@ export default function Testimonials() {
               </div>
             </div>
           </div>
+          </div>
+          <div
+            className={` ${isLarge ? " stickycard1" : " "
+              } `}
+          >
           <div className="medecombg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 mt-10 max-xl:p-10 group rounded-xl">
             <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
             <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
               <div className="h-full w-full">
                 <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
-                  Jeevshakti
+                  Medecom
                 </h1>
                 <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
-                  Jeevshakti Foundation, a charitable trust established in 2017,
-                  is committed to the welfare of stray animals in Delhi. Through
-                  initiatives like feeding, medical care, and sterilizations,
-                  they aim to improve the lives of these animals. Their
-                  innovative solutions and community involvement reflect their
-                  dedication to alleviating animal suffering and promoting
-                  compassion.
+                Medecom is a renowned provider of medical imaging software, specializing in Radiology and Mammography since 2000. Their solutions seamlessly integrate into radiology centers, clinics, and hospitals, offering ergonomic interfaces, optimized workflows, and secure exchanges. Medecom's mission is to facilitate study acquisition, diagnosis, and communication within radiology departments, ensuring efficient and effective processes.
                 </p>
                 <div className="text-center mt-3">
                   <a
@@ -151,10 +173,10 @@ export default function Testimonials() {
               </div>
             </div>
           </div>
-        </Narrow>
+          </div>
       </div>
 
-      <Getintouch/>
+      <Getintouch />
     </div>
   );
 }
