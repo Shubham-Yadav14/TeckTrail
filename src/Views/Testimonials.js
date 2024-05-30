925fd383e872058b3038804c6d2bc99412a89149
Madddynaks <madhav.nakra2002@gmail.com>
2024-05-30 14:49:20 +0530

    minor changes

--- src/Views/Testimonials.js
+++ src/Views/Testimonials.js
@@ -58,14 +58,14 @@ export default function Testimonials() {
             className={` ${isLarge ? " stickycard1" : " "
               } `}
           >
-            <div className="jeevshaktibg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 max-xl:p-10 group rounded-xl">
+            <div className="jeevshaktibg relative  bg-cover bg-center bg-no-repeat h-[100vh] max-md:h-[40vh] p-40 max-xl:p-1 group rounded-xl">
               <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
-              <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
+              <div className="relative z-1 hidden group-hover:block transition-opacity duration-300 rounded-xl">
                 <div className="h-full w-full">
-                  <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
+                  <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center ">
                     Jeevshakti
                   </h1>
-                  <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
+                  <p className="text-white text-2xl max-xl:text-sm font-semibold mt-3 text-center leading-10">
                     Jeevshakti Foundation, a charitable trust established in 2017,
                     is committed to the welfare of stray animals in Delhi. Through
                     initiatives like feeding, medical care, and sterilizations,
@@ -78,7 +78,7 @@ export default function Testimonials() {
                     <a
                       href="http://jeevshaktifoundation.org/"
                       target="_blank"
-                      className="bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-full"
+                      className="bg-blue-600 text-white text-xl max-xl:text-base font-bold px-4 py-2 rounded-full"
                     >
                       Visit Website
                     </a>
@@ -91,21 +91,21 @@ export default function Testimonials() {
             className={` ${isLarge ? " stickycard1" : " "
               } `}
           >
-          <div className="medecombg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 mt-10 max-xl:p-10 group rounded-xl">
+          <div className="medecombg relative  bg-cover max-xl:bg-left bg-center bg-no-repeat h-[100vh] max-md:h-[40vh] p-40 mt-10 max-xl:p-1 group rounded-xl">
             <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
-            <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
+            <div className="relative z-1 hidden group-hover:block transition-opacity duration-300 rounded-xl">
               <div className="h-full w-full">
-                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
+                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center ">
                   Medecom
                 </h1>
-                <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
+                <p className="text-white text-2xl max-xl:text-sm font-semibold mt-3 text-center leading-10">
                 Medecom is a renowned provider of medical imaging software, specializing in Radiology and Mammography since 2000. Their solutions seamlessly integrate into radiology centers, clinics, and hospitals, offering ergonomic interfaces, optimized workflows, and secure exchanges. Medecom's mission is to facilitate study acquisition, diagnosis, and communication within radiology departments, ensuring efficient and effective processes.
                 </p>
                 <div className="text-center mt-3">
                   <a
                     href="https://mede-com.com/"
                     target="_blank"
-                    className="bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-full"
+                    className="bg-blue-600 max-xl:text-base text-white text-xl font-bold px-4 py-2 rounded-full"
                   >
                     Visit Website
                   </a>
@@ -118,14 +118,14 @@ export default function Testimonials() {
             className={` ${isLarge ? " stickycard1" : " "
               } `}
           >
-          <div className="jeevshaktibg mt-10 relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 max-xl:p-10 group rounded-xl">
+          <div className="jeevshaktibg mt-10 relative  bg-cover bg-center bg-no-repeat h-[100vh] max-md:h-[40vh] p-40 max-xl:p-1 group rounded-xl">
             <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
-            <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
+            <div className="relative z-1 hidden group-hover:block transition-opacity duration-300 rounded-xl">
               <div className="h-full w-full">
-                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
+                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center">
                   Jeevshakti
                 </h1>
-                <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
+                <p className="text-white text-2xl max-xl:text-sm font-semibold mt-3 text-center leading-10">
                   Jeevshakti Foundation, a charitable trust established in 2017,
                   is committed to the welfare of stray animals in Delhi. Through
                   initiatives like feeding, medical care, and sterilizations,
@@ -138,7 +138,7 @@ export default function Testimonials() {
                   <a
                     href="http://jeevshaktifoundation.org/"
                     target="_blank"
-                    className="bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-full"
+                    className="bg-blue-600 max-xl:text-base text-white text-xl font-bold px-4 py-2 rounded-full"
                   >
                     Visit Website
                   </a>
@@ -151,21 +151,21 @@ export default function Testimonials() {
             className={` ${isLarge ? " stickycard1" : " "
               } `}
           >
-          <div className="medecombg relative  bg-cover bg-center bg-no-repeat h-[90vh] max-xl:h-[70vh] p-40 mt-10 max-xl:p-10 group rounded-xl">
+          <div className="medecombg relative  bg-cover bg-center bg-no-repeat h-[100vh] max-md:h-[40vh] p-40 mt-10 max-xl:p-1 group rounded-xl">
             <div className="absolute inset-0 bg-black opacity-5 group-hover:opacity-60 transition-opacity duration-300 rounded-xl"></div>
-            <div className="relative z-10 hidden group-hover:block transition-opacity duration-300 rounded-xl">
+            <div className="relative z-1 hidden group-hover:block transition-opacity duration-300 rounded-xl">
               <div className="h-full w-full">
-                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center mt-10">
+                <h1 className="text-white text-6xl max-xl:text-3xl font-bold text-center">
                   Medecom
                 </h1>
-                <p className="text-white text-3xl max-xl:text-lg font-semibold mt-3 text-center leading-10">
+                <p className="text-white text-2xl max-xl:text-sm font-semibold mt-3 text-center leading-10">
                 Medecom is a renowned provider of medical imaging software, specializing in Radiology and Mammography since 2000. Their solutions seamlessly integrate into radiology centers, clinics, and hospitals, offering ergonomic interfaces, optimized workflows, and secure exchanges. Medecom's mission is to facilitate study acquisition, diagnosis, and communication within radiology departments, ensuring efficient and effective processes.
                 </p>
                 <div className="text-center mt-3">
                   <a
                     href="https://mede-com.com/"
                     target="_blank"
-                    className="bg-blue-600 text-white text-xl font-bold px-4 py-2 rounded-full"
+                    className="bg-blue-600 max-xl:text-base text-white text-xl font-bold px-4 py-2 rounded-full"
                   >
                     Visit Website
                   </a>
